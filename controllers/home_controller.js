be5bccda7fdba611c1e92212dd8c93a5e950db6f
TUSHAR PAIGOWAL <tusharpaggowal31@gmail.com>
2021-01-29 14:26:57 +0530

    display comment and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,19 +5,26 @@ module.exports.home=function(req,res){
    // return res.end('<h1>Express is up for codeial</h1>');
    //error in this
    //populate the user of each post
-   Post.find({}).populate('user').exec(function(err,posts){
+   Post.find({})
+   .populate('user')
+   .populate({
+       path:'comments',
+       populate:{
+           path:'user'
+       }
+   })
+   .exec(function(err,posts){
+    
     if(err){
-        console.log("Error in fetching post from database");
+     console.log("Error in fetching post from database");
     }
+    
     console.log(posts);
-     return res.render('home',{
-        title:"connectUs | Home",
-        posts:posts
+    return res.render('home',{
+    title:"connectUs | Home",
+    posts:posts
     });
-   });
-
-
-
+  });
 }
 
 //module.exports.actionName=function(req,res){};
